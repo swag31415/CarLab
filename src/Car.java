59e56dc7ef888341b1b87da54d25fbd0317af952
Swag31415 <Swag31415@LAPTOP-893CEHDL.apsk12.org>
2017-07-18 19:43:20 -0400

    All done!

--- src/Car.java
+++ src/Car.java
@@ -2,10 +2,10 @@
 public class Car {
 
 	// Variable Declarations
-	private int speed;
+	protected int speed;
 	private String make, model, color;
-	private boolean isStarted;
-	private boolean isInUse;
+	protected boolean isStarted;
+	protected boolean isInUse;
 
 	// Constructor
 	public Car(String make, String model, String color) {
@@ -111,37 +111,5 @@ public class Car {
 		return model;
 	}
 	
-	public class SportsCar extends Car{
 
-		public SportsCar(String make, String model, String color) {
-			super(make, model, color);
-		}
-		public void acclerate() {
-			if (isStarted) {
-				if (speed < 80) {
-					speed = speed + 8;
-				} else if (speed < 85) {
-					speed = 85;
-					System.out.println("You are now at the speed limit of 85 mph");
-				} else {
-					System.out.println("You are at the speed limit, it is not proper to go any faster");
-				}
-			}else {
-				System.out.println("Please first start your car");
-			}
-		}
-
-		public void declerate() {
-			if (isStarted) {
-				if (speed > 0) {
-					speed = speed - 5;
-				} else {
-					System.out.println("Sorry dude, but braking at 0mph is really dumb");
-				}
-			}else {
-				System.out.println("Please first start your car");
-			}
-		}
-
-	}	
 }
