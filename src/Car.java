5d2ab59e68de39c2bd4427a028a1b69e43fbb076
Swag31415 <Swag31415@DESKTOP-0R31VTV>
2017-07-18 15:19:58 -0400

    tried SportsCar

--- src/Car.java
+++ src/Car.java
@@ -4,15 +4,17 @@ public class Car {
 	// Variable Declarations
 	private int speed;
 	private String make, model, color;
-	private boolean isStarted, isInUse;
+	private boolean isStarted;
+	private boolean isInUse;
 
 	// Constructor
 	public Car(String make, String model, String color) {
 		this.make = make;
 		this.model = model;
 		this.color = color;
-		isStarted = false;
+		setStarted(false);
 		isInUse = false;
+		setSpeed(0);
 	}
 
 	/**
@@ -32,27 +34,35 @@ public class Car {
 	// <Basic car controls
 	// ---------------------------------------------------------------------
 	public void acclerate() {
-		if (speed < 80) {
-			this.speed += 5;
-		} else if (speed < 85) {
-			this.speed = 85;
-			System.out.println("You are now at the speed limit of 85 mph");
-		} else {
-			System.out.println("You are at the speed limit, it is not proper to go any faster");
+		if (isStarted()) {
+			if (getSpeed() < 80) {
+				this.setSpeed(this.getSpeed() + 5);
+			} else if (getSpeed() < 85) {
+				this.setSpeed(85);
+				System.out.println("You are now at the speed limit of 85 mph");
+			} else {
+				System.out.println("You are at the speed limit, it is not proper to go any faster");
+			}
+		}else {
+			System.out.println("Please first start your car");
 		}
 	}
 
 	public void declerate() {
-		if (speed > 0) {
-			this.speed -= 1;
-		} else {
-			System.out.println("Sorry dude, but braking at 0mph is really dumb");
+		if (isStarted()) {
+			if (getSpeed() > 0) {
+				this.setSpeed(this.getSpeed() - 1);
+			} else {
+				System.out.println("Sorry dude, but braking at 0mph is really dumb");
+			}
+		}else {
+			System.out.println("Please first start your car");
 		}
 	}
 
 	public void startCar() {
-		if (!isStarted) {
-			this.isStarted = true;
+		if (!isStarted()) {
+			this.setStarted(true);
 			System.out.println("You started your car!");
 		} else {
 			System.out.println("You may not start without stopping");
@@ -60,8 +70,8 @@ public class Car {
 	}
 
 	public void stopCar() {
-		if (isStarted) {
-			this.isStarted = false;
+		if (isStarted()) {
+			this.setStarted(false);
 			System.out.println("You stopped your car!");
 		} else {
 			System.out.println("You may not stop without starting");
@@ -77,6 +87,10 @@ public class Car {
 		}
 	}
 
+	public boolean isInUse() {
+		return isInUse;
+	}
+
 	public void setNotInUse() {
 		if (isInUse) {
 			this.isInUse = false;
@@ -84,8 +98,12 @@ public class Car {
 	}
 
 	public void getStats() {
-		System.out.println("Model: " + model + "\nMake: " + make + "\nColor: " + color + "\nSpeed: " + speed
-				+ "\nis Started? : " + isStarted + "\nis in use? : " + isInUse);
+		System.out.println("Model: " + model + "\nMake: " + make + "\nColor: " + color + "\nSpeed: " + getSpeed()
+				+ "\nis Started? : " + isStarted() + "\nis in use? : " + isInUse);
+	}
+
+	private boolean isStarted() {
+		return isStarted;
 	}
 
 	@Override
---
+++ src/Main2.java
@@ -0,0 +1,113 @@
+
+// Author: Swag31415 | Garage Handler Program
+
+//input all the things!
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main2 {
+
+	@SuppressWarnings("finally")// Java wanted this idk why
+	public static String userStringInput(String y) { //String input with decent error control
+		System.out.println(y);
+		String x = "";
+		try {
+		x = new Scanner(System.in).nextLine();
+		} catch (NoSuchElementException ex) {
+			return userStringInput(y);
+		} finally {
+		return x;
+		}
+	}
+	public static boolean userYesNoInput(String y) { //Very likable boolean input
+		System.out.println(y);
+		boolean x = false;
+		String z = "";
+		try {
+			z = new Scanner(System.in).next().toLowerCase();
+			if (z.contains("y")) {
+				x = true;
+			}else if (z.contains("n")) {
+				x = false;
+			} else {
+				System.out.println("Sorry but we wee expecting a yes or no answer, Please try again");
+				return userYesNoInput(y);
+			}
+		} catch (NoSuchElementException ex) {
+			return userYesNoInput(y);
+		}
+		return x;
+	}
+	
+	
+	@SuppressWarnings("resource")// Java wanted this idk why
+	public static int userIntInput(String y) { // Int input with decent error control
+		System.out.println(y);
+		int x = 0;
+		try {
+			x = new Scanner(System.in).nextInt();
+		} catch (InputMismatchException ex) {
+			System.out.println("Sorry we want numerical input, please try again");
+			return userIntInput(y);
+		}
+		return x;
+	}//Implemented the try/catch thing
+	
+	
+	public static void main(String args[]) {//the actual main
+		List<Car> carList = new ArrayList<Car>();//making the carList or "garage"
+		
+		int numOfCars = userIntInput("How many cars do you own?");
+		
+		for (int x = 1; x <= numOfCars; x++) {//getting details on all the cars
+			
+			String make = userStringInput("Please provide a make for car " + x);
+			String model = userStringInput("Please provide a model for car " + x);
+			String color = userStringInput("Please provide a color for car " + x);
+			boolean isSportsCar = userYesNoInput("Is this car a sportscar?");
+			if (isSportsCar) { 
+				carList.add(new SportsCar(make, model, color));
+			} else {
+			carList.add(new Car(make, model, color));
+			}
+			System.out.println("Good job we did car " + x);
+		}
+		
+		while (true) { // beginning to actually do things to cars
+			//Getting some info out
+			System.out.println("Here are your cars! please choose a car to use \n(Type the number of the position)");
+			System.out.println(carList);
+			//getting the car in use
+			int carChosenIndex = userIntInput("");
+			Car carChosen = carList.get(carChosenIndex - 1);
+			carChosen.setInUse();
+			
+			//staring the car usage experience
+			System.out.println("Yay! here are the current stats of your car");
+			carChosen.getStats();
+			System.out.println("You can now try to start, stop, accelerate, decelrate, or get the stats of your car");
+			System.out.println("The correct way to do all that is just by typing these words");
+			while (carChosen.isInUse()) {
+				String userCommand = userStringInput("(start, stop, acc, dec, stats)");
+				if (userCommand.equals("start")) {
+					carChosen.startCar();
+				} else if (userCommand.equals("stop")) {
+					carChosen.stopCar();
+					boolean willUse = userYesNoInput("Would you like to keep using this car? (y/n)");
+					if (!willUse){
+						carChosen.setNotInUse();// Stops the while here
+					}
+				} else if (userCommand.equals("acc")) {
+					carChosen.acclerate();
+				} else if (userCommand.equals("dec")) {
+					carChosen.declerate();
+				} else if (userCommand.equals("stats")) {
+					carChosen.getStats();
+				}
+			}
+		}
+	}
+}
